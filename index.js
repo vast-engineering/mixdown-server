b680e5d53b77a8a6b8196db87a47084688b753b5
Radovan Svedic <radovans@vast.com>
2016-09-06 13:45:26 +0200

    Start sending count of died workers to Graphite

--- index.js
+++ index.js
@@ -117,9 +117,9 @@ Main.prototype.start = function (callback) {
 
           // start logging to Graphite when this happens:
           if (!metrics && Metrics) {
-            var metricsOptions = this.mixdown.apps.carfax.config.plugins.metrics.options;
+            var metricsOptions = this.mixdown.services[0].plugins.metrics.options;
             // environment shouldn't be app id but something is wrong with mixdown apps (env, always undefined) so I just added it there that dummy way too...
-            var metricsID = {appName: this.mixdown.apps.carfax.config.id + '.' + this.mixdown.apps.carfax.config.id, environment: 'undefined'};
+            var metricsID = {appName: this.mixdown.services[0].id + '.' + this.mixdown.services[0].id, environment: 'undefined'};
 
             var metrics = new Metrics(_.extend(metricsID, metricsOptions));
           }
